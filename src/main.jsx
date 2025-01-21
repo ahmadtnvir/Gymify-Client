5012fa114cd07b2ddf2e9ff97cf00ab8fd979454
Tanvir Ahmad <tnvir9@gmail.com>
2025-01-21 12:08:34 +0600

    Install firebase.Add and initialize the Authentication SDK.

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router.jsx";
 import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./context/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </AuthProvider>
   </StrictMode>
 );
